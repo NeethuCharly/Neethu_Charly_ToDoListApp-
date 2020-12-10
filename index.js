5fd936ab593ba054f0cc39b9629c22e01e5a02be
NeethuCharly <72656317+NeethuCharly@users.noreply.github.com>
2020-12-10 16:15:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-let username=document.getElementById("Username");
-let pwd=document.getElementById("Password");
-let usernameerror=document.getElementById("usernameerror");
-let pwderror=document.getElementById("pwderror");
-let flag=0;
-function validation()
-{
-let user=/^(admin)$/
-let exp=/^(12345)$/
-    if(username.value.match(user)){
-        usernameerror.innerHTML="Valid";
-        usernameerror.style.color="green";
-        flag=flag+1;
-       
-    }
-    else{
-        usernameerror.innerHTML="Invalid!";
-        usernameerror.style.color="red";
-        flag=0;
-    }
+const flag={
+    unameflag:true,
+    passwordflag:true
 
-    if(pwd.value.match(exp)){
-        pwderror.innerHTML="Valid";
-        pwderror.style.color="green";
-        flag=flag+1;
-       
-    }
-    else{
-        pwderror.innerHTML="Invalid!";
-        pwderror.style.color="red";
-        flag=0;
-    }   
-
-  if(flag==2){
-      return true;
-  }
-  else{
-      return false;
-  }
 }
+const validate=(el,value)=>{
+    let flagName=el+"flag";
+    if($(`#${el}`).val()===value){
+        flag[flagName]=false;
+        const msg='';
+        $(`#${el}Error`).html(msg);
+        $(`#${el}Error`).addClass('hidden');
+    }
+    else{
+        flag[flagName]=true;
+        const msg=`Invalid  ${el} `;
+        $(`#${el}Error`).html(msg);
+        $(`#${el}Error`).removeClass('hidden');
+       
+    }
+
+}
+
+$('#uname').on('input',(e)=>{validate('uname','admin')});
+$('#password').on('input',(e)=>{validate('password','12345')});
+
+const redirectAndAuthenticate=()=>{
+    window.localStorage.setItem('user',JSON.stringify({uname:'admin'}));
+    window.location='home.html';
+}
+const loginAction=(e,callback)=>{
+    if(!flag['unameflag'] && !flag['passwordflag']){
+        e.preventDefault();
+        callback();
+    }
+}
+$('#login').on('click',(e)=>{
+    loginAction(e,redirectAndAuthenticate);
+}
+
+) 
